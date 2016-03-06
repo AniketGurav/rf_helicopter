dc7dbc18238a3692978c825efb7e4f4dfc8ac20f
Dan <dan.dixey@gmail.com>
2016-03-06 00:24:45 +0000

    Adding Plots

--- Plot_results.py
+++ Plot_results.py
@@ -13,7 +13,10 @@ import numpy as np
 import os
 import json
 import logging
+from random import choice
 from Model import World as W
+from sklearn.preprocessing import normalize
+from scipy.interpolate import spline
 plt.style.use('ggplot')
 
 
@@ -24,113 +27,115 @@ logging.basicConfig(format='[%(asctime)s] : [%(levelname)s] : [%(message)s]',
 
 logging.info('Set Script Parameters')
 case_name = 'case_one'
-model = str(1)
+model = str(2)
 directory = os.path.join(os.getcwd(), 'Results',
-                         case_name, 'Model{}.json'.format(model))
+                         case_name)
 
 
 logging.info('Reading Data from File')
-data = json.loads(open(directory, 'r').read())
+data = json.loads(open(directory + '/Model{}.json'.format(model), 'r').read())
 HeliWorld = W.helicopter_world(file_name="Track_1.npy")
+xlim_val = int(data['model_names'][0].split('trails_')[1].split('_nb')[0])
+nb_action = int(data['model_names'][0].split(
+    'nb_actions_')[1].split('_model_')[0])
 
-# fig = plt.figure()
-# fig.canvas.draw()
-# plt.subplot(2, 2, 1)
-# plt.title('Best Realized Patter', fontsize=10)
-# plt.xlabel('Track Length', fontsize=8)
-# plt.ylabel('Track Width', fontsize=8)
-# my_axis = plt.gca()
-# my_axis.set_xlim(0, HeliWorld.track_width)
-# my_axis.set_ylim(0, HeliWorld.track_height)
-# im1 = plt.imshow(HeliWorld.track,
-#                  cmap=plt.cm.jet,
-#                  interpolation='nearest',
-#                  vmin=-1,
-#                  vmax=8)
-# plt.colorbar(im1, fraction=0.01, pad=0.01)
-#
-# plt.subplot(2, 2, 2)
-# plt.title('Final Q Matrix', fontsize=10)
-# plt.xlabel('Track Length', fontsize=8)
-# plt.ylabel('Track Width', fontsize=8)
-# my_axis = plt.gca()
-# my_axis.set_xlim(0, HeliWorld.track_width)
-# my_axis.set_ylim(0, HeliWorld.track_height)
-# a = np.zeros(shape=(HeliWorld.track_height,
-#                     HeliWorld.track_width))
-# im2 = plt.imshow(a)
-# plt.colorbar(im2, fraction=0.01, pad=0.01)
-#
-# plt.subplot(2, 2, 3)
-# plt.title('Completion Time Chart', fontsize=10)
-# plt.xlabel('Trial Numbers', fontsize=8)
-# plt.ylabel('Seconds Per Trial', fontsize=8)
-# my_axis = plt.gca()
-# my_axis.set_xlim(0, settings['trials'])
-#
-# plt.subplot(2, 2, 4)
-# plt.title('Learning Chart', fontsize=10)
-# plt.xlabel('Trial Numbers', fontsize=8)
-# plt.ylabel('End Location', fontsize=8)
-# my_axis = plt.gca()
-# my_axis.set_xlim(0, settings['trials'])
-# my_axis.set_ylim(0, 1)
-#
-# colors = ['black', 'green', 'red', 'cyan', 'magenta',
-#           'yellow', 'blue', 'white', 0.3, 0.55, 0.85]
-#
-#
-#         plt.subplot(2, 2, 4)
-#                 # Final Location Plot
-#                 plt.scatter(HeliWorld.trials,
-#                             Helicopter1.final_location[-1][0] /
-#                             float(HeliWorld.track_width),
-#                             s=np.pi * (1 * 1) ** 2,
-#                             c=colors[value_iter],
-#                             alpha=0.5)
-#                 plt.legend()
-#                 # Completion Time Chart
-#                 plt.subplot(2, 2, 3)
-#                 plt.scatter(value[0],
-#                             value[1],
-#                             s=np.pi * (1 * 1) ** 2,
-#                             c=colors[value_iter],
-#                             alpha=0.5)
-#
-# fig.suptitle('Time for Trial Completion: {} - Current State: {} - Current Location: {}\n\
-#                              Trials Completed: {} with Total Time {:.3f} seconds\n\
-#                              Agent Model: {} \n\
-#                              Agent Parameters: alpha {} - epsilon {:.4f} - gamma {} - Number of Actions: {}\n\
-#                              World Paramers: Length of Track: {} - Width of Track: {} - CASE: {}'.format(
-#                     HeliWorld.trials,
-#                     Helicopter1.current_state,
-#                     Helicopter1.current_location,
-#                     value[0],
-#                     time() - st + 1e-9,
-#                     Utils.titles[settings['model'] - 1],
-#                     settings['alpha'],
-#                     settings['epsilon'],
-#                     settings['gamma'],
-#                     settings['nb_actions'],
-#                     HeliWorld.track_width,
-#                     HeliWorld.track_height,
-#                     case),
-#                     fontsize=10,
-#                     horizontalalignment='center',
-#                     verticalalignment='top')
-#
-#                 # Plotting Real-time plot
-#                 plt.subplot(2, 2, 1)
-#                 plt.imshow(HeliWorld.track,
-#                            cmap=plt.cm.jet,
-#                            interpolation='nearest',
-#                            vmin=-1,
-#                            vmax=8)
-#                 plt.scatter(Helicopter1.current_location[0],
-#                             Helicopter1.current_location[1],
-#                             s=np.pi * (1 * 1) ** 2,
-#                             c=colors[value_iter])
-#
-#                 plt.subplot(2, 2, 2)
-#                 plt.imshow(a)
-#                 plt.pause(1e-10)
+# Plotting Colors
+colors = ['coral', 'green', 'red', 'cyan', 'magenta',
+          'yellow', 'blue', 'white', 'fuchsia', 'orangered', 'steelblue']
+
+logging.info('Trained Plot - Epsilon set to 0')
+fig = plt.figure()
+fig.canvas.draw()
+plt.subplot(2, 2, 1)
+plt.title('Post Training Path - Epsilon = 0',
+          fontsize=10)
+plt.xlabel('Track Length', fontsize=8)
+plt.ylabel('Track Width', fontsize=8)
+my_axis = plt.gca()
+my_axis.set_xlim(0, HeliWorld.track_width)
+my_axis.set_ylim(0, HeliWorld.track_height)
+im1 = plt.imshow(HeliWorld.track,
+                 cmap=plt.get_cmap('gray'),
+                 interpolation='nearest',
+                 vmin=-1,
+                 vmax=8)
+plt.colorbar(im1, fraction=0.01, pad=0.01)
+
+# For each set of results in dictionary
+for i in range(len(data['best_test'])):
+    x, y = [], []
+    for each_item in data['best_test'][i]:
+        x.append(each_item[0])
+        y.append(each_item[1])
+    # Plot Scatter
+    plt.scatter(x=x,
+                y=y,
+                s=np.pi * (1 * 1) ** 2,
+                c=colors[i])
+
+logging.info('Plotting the Q-Matrix')
+plt.subplot(2, 2, 2)
+selection = choice(range(len(data['best_test'])))
+plt.title('Final Q Matrix', fontsize=10)
+plt.xlabel('Track Length', fontsize=8)
+plt.ylabel('Track Width', fontsize=8)
+my_axis = plt.gca()
+my_axis.set_xlim(0, HeliWorld.track_width)
+my_axis.set_ylim(0, HeliWorld.track_height)
+q_data = np.array(data['q_plot'][selection])
+im2 = plt.imshow(normalize(q_data))
+plt.colorbar(im2, fraction=0.01, pad=0.01)
+
+logging.info('Completion Chart - Time per Trial')
+plt.subplot(2, 2, 3)
+plt.title('Completion Chart - Time per Trial', fontsize=10)
+plt.xlabel('Trial Numbers', fontsize=8)
+plt.ylabel('LOG(Seconds Per Trial)', fontsize=8)
+my_axis = plt.gca()
+my_axis.set_xlim(0, xlim_val)
+
+# For each set of results in dictionary
+for i in range(len(data['time_chart'])):
+    x, y = [], []
+    for each_item in data['time_chart'][i]:
+        x.append(each_item[0])
+        y.append(each_item[1])
+    # Plot Scatter
+    plt.scatter(x=x,
+                y=np.log(y),
+                s=np.pi * (1 * 1) ** 2,
+                c=colors[i])
+
+plt.subplot(2, 2, 4)
+plt.title('Learning Chart - Averaged Trial Plot', fontsize=10)
+plt.xlabel('Trial Numbers', fontsize=8)
+plt.ylabel('End Location', fontsize=8)
+
+
+def moving_average(interval, window_size):
+    window = np.ones(int(window_size)) / float(window_size)
+    return np.convolve(interval, window, 'same')
+
+# For each set of results in dictionary
+for i in range(len(data['final_location'])):
+    x, y = [], []
+    for each_item in data['final_location'][i]:
+        x.append(each_item[0])
+        y.append(each_item[1])
+
+    y = moving_average(y, 60)
+
+    plt.plot(x, y, linewidth=1, c=colors[i])
+
+logging.info('Plotting Figure Label')
+fig.suptitle(
+    '|| Case - {} | Number of Trials - {} | Model - {} | Number of Actions - {} ||\n\
+              || TRACK | Width - {} | Height - {} ||'.format(
+        case_name,
+        xlim_val,
+        model,
+        nb_action,
+        HeliWorld.track_width,
+        HeliWorld.track_height))
+logging.info('Saved Figure of the Plot')
+fig.savefig(directory + '/Plot/Final_Plot.png')
